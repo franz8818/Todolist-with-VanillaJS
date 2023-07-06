247901aedca081cd36bbdf8ef24ada03d0d2c5cb
Franz Seidel <franz8818@gmail.com>
2023-07-06 21:51:27 +0000

    quedamos en la función de agregar tarea

--- app.js
+++ app.js
@@ -9,12 +9,24 @@ console.log(botonAgregar);
 const listaTareasDom = document.getElementsByName("todo-list");
 console.log(listaTareasDom);
 
+//variable inmutable nuevaTarea = "string vacio -> para que agrege el valor del usuario";
+let nuevaTarea = "";
+let listaTareas = []; //arreglo vacio -> "lista de cosas"
+
 //function nombreFuncion() { bloque de codigo }
 //Un parámetro es un valor dinámico ->  función (parámetro) -> (evento)
 function actualizaTarea(evento) {
-    //consola(parámetro.objetivo.valor de lo que el usuario escribe -> propiedad del TARGET)
-    console.log(evento.target.value);
+    //Permite asignarle a la nueva al STRING vacio un nuevo valor (lo que el usuario vaya escribiendo) 
+    nuevaTarea = evento.target.value
+    console.log(nuevaTarea);
+}
+//Se crea función para ver si esta funcionado.
+function agregarTarea() {
+    console.log("Hola Franz");
 }
 
 // elemento.addEventListener ("tipo de evento", (parámetro) arrowfunction function(parámetro))
-inputTodo.addEventListener("change", (event) => actualizaTarea(event))
+inputTodo.addEventListener("change", (event) => actualizaTarea(event));
+
+//elemento.addEventListener ("usuario hace click con el mouse", (parámetro) arrowfunction function(parámetro))
+botonAgregar.addEventListener("click", agregarTarea);
